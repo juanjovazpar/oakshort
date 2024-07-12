92909880c144420655e9863227187d21d82eb974
Juanjo <juanjovazpar@gmail.com>
2024-07-12 21:52:32 +0200

    feat(api):Clean up short controller

--- src/backend/controllers/short.controller.ts
+++ src/backend/controllers/short.controller.ts
@@ -1,28 +1,5 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { Short } from '../models/short.model';
-import { PARAMS } from '../../backend/routes';
-
-export const redirectShort = async (req: FastifyRequest, res: FastifyReply) => {
-  try {
-    const { shorten_id } = req.params as {
-      [PARAMS.SHORTEN_ID]: string;
-    };
-
-    const short = await Short.findByIdAndUpdate(
-      { short: shorten_id },
-      { $inc: { accessCount: 1, lastRead: Date.now() } },
-      { new: true }
-    );
-
-    if (!short || !short.target || !short.active || short.deleted) {
-      return res.status(404).send({ message: 'Invalid short' });
-    }
-
-    res.redirect(short.target);
-  } catch (error: any) {
-    res.send(error);
-  }
-};
 
 export const updateShort = async (
   req: FastifyRequest<{ Body: any }>,
