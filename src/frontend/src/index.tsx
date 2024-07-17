bb3f7d15ecf49af328b38398c6ba7db6eb4af73b
Juanjo <juanjovazpar@gmail.com>
2024-07-17 20:40:06 +0200

    feat(ui):Add layout store basic version

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Layout from './views/Layout';
+import store from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Layout />
+    <Provider store={store}>
+      <Layout />
+    </Provider>
   </React.StrictMode>
 );
 
--- src/frontend/src/views/Layout.tsx
+++ src/frontend/src/views/Layout.tsx
@@ -1,22 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import './Layout.css';
 import Hello from './hello/Hello';
 import Dashboard from './dashboard/Dashboard';
 import Sidebar from './sidebar/Sidebar';
+import {
+  toggleCollapse,
+  toggleCollapseSide,
+} from '../store/layout/layout.slice';
 
 function Layout() {
-  const [isCollapsed, setIsCollapsed] = useState(true);
-  const [isCollapsedSide, setIsCollapsedSide] = useState(false);
+  const dispatch = useDispatch();
+  const isCollapsed = useSelector((state: any) => state.layout.isCollapsed);
+  const isCollapsedSide = useSelector(
+    (state: any) => state.layout.isCollapsedSide
+  );
 
   return (
     <div className={`layout ${isCollapsed ? 'collapse' : ''}`}>
       <section className="hello-section collapse">
         <Hello />
-        <button onClick={() => setIsCollapsed(!isCollapsed)}>
+        <button onClick={() => dispatch(toggleCollapse())}>
           {isCollapsed ? 'Expand' : 'Collapse'}
         </button>
-        <button onClick={() => setIsCollapsedSide(!isCollapsedSide)}>
+        <button onClick={() => dispatch(toggleCollapseSide())}>
           {isCollapsedSide ? 'Expand Side' : 'Collapse Side'}
         </button>
       </section>
