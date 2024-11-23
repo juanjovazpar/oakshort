d04e054d7bc46ee307c24868228ecb86e0708c8c
Juanjo <juanjovazpar@gmail.com>
2024-11-23 10:31:17 +0100

    feat(TG-52 #closed): Add Autoload for routes and plugins

---
+++ src/backend/app/index.ts
@@ -0,0 +1,24 @@
+import { FastifyInstance } from 'fastify';
+import AutoLoad from '@fastify/autoload';
+import * as path from 'path';
+
+/* eslint-disable-next-line */
+export interface AppOptions {}
+
+export async function app(fastify: FastifyInstance, opts: AppOptions) {
+  // TODO: Use fastify.env to set environment variables
+
+  // This loads all plugins defined in plugins folder
+  // define your plugins in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: { ...opts },
+  });
+
+  // This loads all routes defined in routes folder
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'routes'),
+    options: { ...opts },
+  });
+}
--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,100 +1,22 @@
 import Fastify from 'fastify';
-import { AddressInfo } from 'net';
-import cors from '@fastify/cors';
-import dotenv from 'dotenv';
-import fastifyEnv from '@fastify/env';
-import jwt from '@fastify/jwt';
-import bcrypt from 'fastify-bcrypt';
-import mongoose from 'mongoose';
-import path from 'path';
+import { app } from './app';
 
-import AuthRoute from './routes/auth.route';
-import RedirectRoute from './routes/redirect.route';
-import ShortsRoute from './routes/shorts.route';
-import ShortRoute from './routes/short.route';
-import { errorHandler } from './middlewares/error.middleware';
-import { requestLogger } from './middlewares/requestLogger.middleware';
-import { authMiddleware } from './middlewares/auth.middleware';
-import { shortMiddleware } from './middlewares/short.middleware';
+const host = process.env.HOST ?? '0.0.0.0';
+const port = process.env.API_AUTH_PORT
+  ? Number(process.env.API_AUTH_PORT)
+  : 3000;
 
-const envPath = path.resolve(__dirname, '../../.env');
-dotenv.config({ path: envPath });
-
-declare module 'fastify' {
-  interface FastifyInstance {
-    config: {
-      PORT_BE: string;
-      DB_URI: string;
-      JWT_SECRET: string;
-    };
-    authenticate: typeof authMiddleware;
-    short: typeof shortMiddleware;
-  }
-}
-
-const app = Fastify({
+const server = Fastify({
   logger: true,
 });
 
-const schema = {
-  type: 'object',
-  required: ['PORT_BE'],
-  properties: {
-    PORT_BE: {
-      type: 'string',
-      default: '80',
-    },
-    DB_URI: { type: 'string' },
-    JWT_SECRET: { type: 'string' },
-  },
-};
-const envOptions = {
-  confKey: 'config',
-  schema: schema,
-  dotenv: true,
-  data: process.env,
-};
+server.register(app);
 
-mongoose
-  .connect(process.env.DB_URI!)
-  .then(() => {
-    app.log.info('Connected to MongoDB');
-  })
-  .catch((error) => {
-    app.log.info('Error  connecting to MongoDB:', error);
-  });
-
-app.register(fastifyEnv, envOptions);
-app.register(cors);
-app.register(bcrypt, { saltWorkFactor: 12 });
-app.register(jwt, {
-  secret: process.env.JWT_SECRET!,
-});
-
-// Middlewares
-app.decorate('authenticate', authMiddleware);
-app.decorate('short', shortMiddleware);
-// app.addHook('preHandler', requestLogger);
-app.setErrorHandler(errorHandler);
-
-// Routes
-app.register(AuthRoute);
-app.register(ShortsRoute);
-app.register(ShortRoute);
-app.register(RedirectRoute);
-
-const start = async () => {
-  try {
-    const port = Number(process.env.PORT_BE) || 80;
-    await app.listen({ port });
-    const address = app.server.address();
-    const portNumber =
-      typeof address === 'string' ? address : (address as AddressInfo).port;
-    app.log.info(`Server listening on ${portNumber}`);
-  } catch (err) {
-    app.log.error(err);
+server.listen({ port, host }, (err) => {
+  if (err) {
+    server.log.error(err);
     process.exit(1);
+  } else {
+    console.log(`[ ready ] http://${host}:${port}`);
   }
-};
-
-start();
+});
